a993fe55ff082592e4f46ca758a44d976cd5c995
inxups <v>
2026-07-30 15:42:38 +0800

    feat: reset underworld generation baseline

--- src/main/java/com/pixulse/infx/datagen/ModBiomeTagsProvider.java
+++ src/main/java/com/pixulse/infx/datagen/ModBiomeTagsProvider.java
@@ -2,7 +2,6 @@ package com.pixulse.infx.datagen;
 
 import com.pixulse.infx.InfiniteX;
 import com.pixulse.infx.world.RiverBiomes;
-import com.pixulse.infx.world.Underworld;
 import java.util.concurrent.CompletableFuture;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
@@ -11,7 +10,7 @@ import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.biome.Biome;
 import org.jspecify.annotations.NonNull;
 
-/** Moves the shallow Overworld's subterranean structures into the Underworld biome. */
+/** Supplies biome tags for custom rivers and the restored stronghold progression. */
 final class ModBiomeTagsProvider extends KeyTagsProvider<Biome> {
     ModBiomeTagsProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider) {
         super(output, Registries.BIOME, lookupProvider, InfiniteX.MOD_ID);
@@ -28,12 +27,6 @@ final class ModBiomeTagsProvider extends KeyTagsProvider<Biome> {
                 .add(RiverBiomes.JUNGLE_RIVER)
                 .add(RiverBiomes.SWAMP_RIVER);
         tag(BiomeTags.IS_JUNGLE).add(RiverBiomes.JUNGLE_RIVER);
-        tag(BiomeTags.HAS_ANCIENT_CITY, true).add(Underworld.BIOME);
-        tag(BiomeTags.HAS_BURIED_TREASURE, true).add(Underworld.BIOME);
-        tag(BiomeTags.HAS_MINESHAFT, true).add(Underworld.BIOME);
-        tag(BiomeTags.HAS_MINESHAFT_MESA, true).add(Underworld.BIOME);
         tag(BiomeTags.HAS_STRONGHOLD).addTag(BiomeTags.IS_OVERWORLD);
-        tag(BiomeTags.HAS_TRAIL_RUINS, true).add(Underworld.BIOME);
-        tag(BiomeTags.HAS_TRIAL_CHAMBERS, true).add(Underworld.BIOME);
     }
 }
