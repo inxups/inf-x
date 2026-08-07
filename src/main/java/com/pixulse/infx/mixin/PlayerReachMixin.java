ee1161f35432eb1005a72af96c2e9b728282b9ff
inxups <v>
2026-08-07 20:06:28 +0800

    update PlayerReach

--- src/main/java/com/pixulse/infx/mixin/PlayerReachMixin.java
+++ src/main/java/com/pixulse/infx/mixin/PlayerReachMixin.java
@@ -3,6 +3,7 @@ package com.pixulse.infx.mixin;
 import com.pixulse.infx.InfiniteXTestMode;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -10,17 +11,24 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 /** InfX creative players reach 5 blocks for block and entity interaction. */
 @Mixin(Player.class)
 public abstract class PlayerReachMixin {
+    @Shadow
+    public abstract boolean isCreative();
+
     @Inject(method = "blockInteractionRange", at = @At("HEAD"), cancellable = true)
     private void blockInteractionRange(CallbackInfoReturnable<Double> callback) {
-        if (!InfiniteXTestMode.isEnabled() && ((Player) (Object) this).getAbilities().instabuild) {
+        if (InfiniteXTestMode.isEnabled() && this.isCreative()) {
             callback.setReturnValue(5.0);
+            return;
         }
+        callback.setReturnValue(2.25);
     }
 
     @Inject(method = "entityInteractionRange", at = @At("HEAD"), cancellable = true)
     private void entityInteractionRange(CallbackInfoReturnable<Double> callback) {
-        if (!InfiniteXTestMode.isEnabled() && ((Player) (Object) this).getAbilities().instabuild) {
+        if (InfiniteXTestMode.isEnabled() && this.isCreative()) {
             callback.setReturnValue(5.0);
+            return;
         }
+        callback.setReturnValue(2.5);
     }
 }
