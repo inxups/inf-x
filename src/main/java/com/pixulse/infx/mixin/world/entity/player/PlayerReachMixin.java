1aab0cd0731714daeb807d7c1ef073baf1700477
inxups <v>
2026-08-11 13:43:43 +0800

    redefine playerreach

--- src/main/java/com/pixulse/infx/mixin/world/entity/player/PlayerReachMixin.java
+++ src/main/java/com/pixulse/infx/mixin/world/entity/player/PlayerReachMixin.java
@@ -1,13 +1,17 @@
 package com.pixulse.infx.mixin.world.entity.player;
 
 import com.pixulse.infx.InfiniteXTestMode;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Objects;
 
 /**
  * InfX creative players reach 5 blocks for block and entity interaction. Survival reach follows
@@ -18,13 +22,16 @@ public abstract class PlayerReachMixin {
     @Shadow
     public abstract boolean isCreative();
 
+    @Shadow
+    public abstract ItemStack getWeaponItem();
+
     @Inject(method = "blockInteractionRange", at = @At("HEAD"), cancellable = true)
     private void blockInteractionRange(CallbackInfoReturnable<Double> callback) {
         if (InfiniteXTestMode.isEnabled() && this.isCreative()) {
             callback.setReturnValue(5.0);
             return;
         }
-        callback.setReturnValue(((Player) (Object) this).getAttributeValue(Attributes.BLOCK_INTERACTION_RANGE));
+        callback.setReturnValue(2.5);
     }
 
     @Inject(method = "entityInteractionRange", at = @At("HEAD"), cancellable = true)
@@ -33,6 +40,6 @@ public abstract class PlayerReachMixin {
             callback.setReturnValue(5.0);
             return;
         }
-        callback.setReturnValue(((Player) (Object) this).getAttributeValue(Attributes.ENTITY_INTERACTION_RANGE));
+        callback.setReturnValue(2.5);
     }
 }
